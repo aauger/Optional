e1451059ce631f569063764493b7566a475cae91
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-03-31 19:32:12 -0400

    Slim optional library to avoid nulls in user code

---
+++ Optional/OptionalTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using Optional;
+
+var myFoo = Option<string>.None;
+var myBar = Option<string>.Some("hello world!");
+
+Action<string> forString = (s) => Console.WriteLine(s);
+
+myFoo.Then(forString);
+myBar.Then(forString);
+
+myFoo.Match(
+    some: (s) => Console.WriteLine(s),
+    none: () => Console.WriteLine("nothing here!")
+);
